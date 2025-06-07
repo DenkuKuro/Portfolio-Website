5213cca8ab33b3542a256b92b88e63ec8fdcc39b
DenkuKuro <javier.deng17@gmail.com>
2025-06-06 17:31:03 -0700

    finished About me Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { selfie, CV } from '../assets'
 import { skills } from '../constants'
+import styles from '../styles'
 import Button from './Button'
 
 const About = () => {
@@ -14,13 +15,18 @@ const About = () => {
         <div className=''>
           <img src={selfie} alt="selfie of me" className='h-96'/>
         </div>
-        <div className='mx-32'>
-          <p className='text-xl max-w-2xl break-words'>
+        <div className='mx-32 py-10'>
+          <p className={'text-xl max-w-2xl break-words'}>
             I am a second-year Simon Fraser Student studying Computer Science aspiring to be a Software Engineer! 
-            I have worked on several web app projects focusing on Full-Stack applications. I love playing volleyball and really interested
-            in learning embedded software engineering in the future.
+            I have worked on several web app projects focusing on Full-Stack applications. I always interestes in learning new techonologies and like to work
+             on my foundations to improve little by little. I love playing volleyball and really interested
+            in learning embedded software engineering in the future. <br />
+            
           </p>
-          <Button link={CV} name={"CV"} style={""}/>
+          <p className='text-xl mt-7'>
+            Here is my Resume if you are interested in checking it:
+          </p>
+          <Button link={CV} name={"CV"} style={"px-8"}/>
         </div>
       </div>
     </section>
