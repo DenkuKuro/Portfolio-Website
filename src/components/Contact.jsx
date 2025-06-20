f91f490796845273fe07f89f939d2109f6abb378
DenkuKuro <javier.deng17@gmail.com>
2025-06-19 18:53:13 -0700

    Styled contact me section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,26 +4,27 @@ import { contact_pic } from '../assets'
 
 const Contact = () => {
   return (
-    <section className='h-screen my-32 p-6 mb-56'>
+    <section className='h-full my-32 p-6 mb-56'>
       <div className=''>
         <h1 className='text-4xl'>Contact Me</h1>
         <div className='w-full border-b-[0.5px] border-gradient border-gradient-red my-2'></div>
       </div>
-      <div className='flex my-15 justify-center h-full items-center gap-20'>
-        <form action="" method='' className='flex flex-col h-3/4 w-1/4 bg-slate-400 justify-center'>
+      <div className='flex mt-32 justify-center h-full items-center gap-20'>
+        <form action="" method='' className='flex flex-col h-2/4 w-2/5 bg-slate-800 justify-center px-8 text-black rounded-md py-10'>
+          <h1 className='text-center text-lg m-0 p-0'>Send me a message!</h1>
           <label for="name" className='w-full'>
             <h5>Name</h5>
-            <input id="name" type="text" className='w-full'/>
+            <input id="name" type="text" className='w-full text-black'/>
           </label>
           <label for="email">
             <h5>Email</h5>
-            <input id="email" type="text" className='w-full'/>
+            <input id="email" type="text" className='w-full text-black'/>
           </label>
           <label for="message">
             <h5>Message</h5>
-            <input id="message" type="" className='w-full'/>
+            <textarea name="message" id="message" rows={5} cols={52} className='text-black'></textarea>
           </label>
-          <button type="submit" class="btn btn-outline-primary">Submit</button>
+          <button type="submit" class={`${styles.btn} ${styles} text-white`}>Submit</button>
         </form>
         <div className=''>
           <img src={contact_pic} alt="selfie of me" className='rounded-full h-72' />
